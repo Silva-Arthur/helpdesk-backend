d2bc18cee59c8bfe1c89a829557d906d933a4f0e
Arthur <pereiras.oarthur@gmail.com>
2023-09-04 17:49:02 -0300

    Ajuste da carga do H2

--- src/main/java/com/devarthursilva/helpdesk/services/UserDetailsServiceImpl.java
+++ src/main/java/com/devarthursilva/helpdesk/services/UserDetailsServiceImpl.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.devarthursilva.helpdesk.domain.Pessoa;
 import com.devarthursilva.helpdesk.repositories.PessoaRepository;
 import com.devarthursilva.helpdesk.security.UserSS;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService{
 	
 	@Autowired
