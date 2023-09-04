2eebdeefabb340ea94f19dafc925602efda1a594
Arthur <pereiras.oarthur@gmail.com>
2023-09-03 21:11:00 -0300

    Adição dos services

--- src/main/java/com/devarthursilva/helpdesk/config/TestConfig.java
+++ src/main/java/com/devarthursilva/helpdesk/config/TestConfig.java
@@ -12,7 +12,7 @@ import com.devarthursilva.helpdesk.services.DBService;
  * 
  * */
 @Configuration
-@Profile("test")
+@Profile("teste")
 public class TestConfig {
 	
 	@Autowired
---
+++ src/main/java/com/devarthursilva/helpdesk/resources/TecnicoResource.java
@@ -0,0 +1,25 @@
+package com.devarthursilva.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devarthursilva.helpdesk.domain.Tecnico;
+import com.devarthursilva.helpdesk.services.TecnicoService;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResource {
+	
+	@Autowired
+	TecnicoService tecnicoService;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+		Tecnico obj = tecnicoService.findById(id);
+		return ResponseEntity.ok(obj);
+	}
+}
